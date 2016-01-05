af6379d3b19da7da13770acae9586fe1a9911f00
Administrator <1363055106@qq.com>
2016-01-05 20:00:47 +0800

    拒绝提交!changes.default.changelist.name!

---
+++ trunk/task13/js/task13.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2016/1/3.
+ */
+function appears() {
+    var s = document.getElementById("sub");
+    if (s.style.display == "block") {
+        s.style.display = "none";
+    }
+    else {
+        s.style.display = "block";
+    }
+}
